89790e1321bc9ebead414b129dd17d5d76912725
tacheraSasi <tacherasasi@gmail.com>
2024-11-23 20:52:22 +0300

    more changes

--- object/object.go
+++ object/object.go
@@ -4,53 +4,59 @@ import (
 	"fmt"
 )
 
+// ObjectType represents various types of objects
 type ObjectType string
 
+// Constants for object types
 const (
-	INTEGER_OBJ      = "NAMBA"
-	FLOAT_OBJ        = "DESIMALI"
+	INTEGER_OBJ      = "INTEGER"
+	FLOAT_OBJ        = "FLOAT"
 	BOOLEAN_OBJ      = "BOOLEAN"
-	NULL_OBJ         = "TUPU"
-	RETURN_VALUE_OBJ = "RUDISHA"
-	ERROR_OBJ        = "KOSA"
-	FUNCTION_OBJ     = "UNDO (FUNCTION)"
-	STRING_OBJ       = "NENO"
-	BUILTIN_OBJ      = "YA_NDANI"
-	ARRAY_OBJ        = "ORODHA"
-	DICT_OBJ         = "KAMUSI"
-	CONTINUE_OBJ     = "ENDELEA"
-	BREAK_OBJ        = "VUNJA"
-	FILE_OBJ         = "FAILI"
-	TIME_OBJ         = "MUDA"
-	JSON_OBJ         = "JSONI"
+	NULL_OBJ         = "NULL"
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
+	ERROR_OBJ        = "ERROR"
+	FUNCTION_OBJ     = "FUNCTION"
+	STRING_OBJ       = "STRING"
+	BUILTIN_OBJ      = "BUILTIN"
+	ARRAY_OBJ        = "ARRAY"
+	DICT_OBJ         = "DICT"
+	CONTINUE_OBJ     = "CONTINUE"
+	BREAK_OBJ        = "BREAK"
+	FILE_OBJ         = "FILE"
+	TIME_OBJ         = "TIME"
+	JSON_OBJ         = "JSON"
 	MODULE_OBJ       = "MODULE"
 	BYTE_OBJ         = "BYTE"
-	PACKAGE_OBJ      = "PAKEJI"
-	INSTANCE         = "PAKEJI"
+	PACKAGE_OBJ      = "PACKAGE"
+	INSTANCE         = "INSTANCE"
 	AT               = "@"
 )
 
+// Object interface represents any object in the system
 type Object interface {
 	Type() ObjectType
 	Inspect() string
 }
 
+// HashKey is used for hashable objects like strings and integers
 type HashKey struct {
 	Type  ObjectType
 	Value uint64
 }
 
+// Hashable interface is for objects that can be used as keys in a hash map
 type Hashable interface {
 	HashKey() HashKey
 }
 
-// Iterable interface for dicts, strings and arrays
+// Iterable interface supports iteration for collections like dicts, strings, and arrays
 type Iterable interface {
 	Next() (Object, Object)
 	Reset()
 }
 
+// newError creates a formatted error object with red text
 func newError(format string, a ...interface{}) *Error {
-	format = fmt.Sprintf("\x1b[%dm%s\x1b[0m", 31, format)
-	return &Error{Message: fmt.Sprintf(format, a...)}
+	redFormat := fmt.Sprintf("\x1b[%dm%s\x1b[0m", 31, format) // Red-colored error message
+	return &Error{Message: fmt.Sprintf(redFormat, a...)}
 }
