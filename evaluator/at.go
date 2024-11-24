30d28b87c11e764e059af07e77c1dc7df09103a1
tacheraSasi <tacherasasi@gmail.com>
2024-11-24 19:34:44 +0300

    vintLang vscode formatter

--- evaluator/at.go
+++ evaluator/at.go
@@ -5,9 +5,14 @@ import (
 	"github.com/ekilie/vint-lang/object"
 )
 
+// evalAt handles the evaluation of the "@" expression.
+// It checks if the "@" symbol is available in the current environment and returns its value.
+// If not found, it returns an error indicating that the scope is invalid.
 func evalAt(node *ast.At, env *object.Environment) object.Object {
+	// Checks if "@" exists in the environment
 	if at, ok := env.Get("@"); ok {
-		return at
+		return at // Return the value associated with "@"
 	}
-	return newError("Iko nje ya scope")
+	// Returns an error if "@" is not found in the scope
+	return newError("Out of scope")
 }
