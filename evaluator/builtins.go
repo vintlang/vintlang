547b7b826ecee466f61c8a9162ab3fad439f9f65
tacheraSasi <tacherasasi@gmail.com>
2024-11-24 17:51:53 +0300

    builtins

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -15,11 +15,11 @@ var builtins = map[string]*object.Builtin{
 		Fn: func(args ...object.Object) object.Object {
 
 			if len(args) > 1 {
-				return newError("Samahani, kiendesha hiki kinapokea hoja 0 au 1, wewe umeweka %d", len(args))
+				return newError("Sorry, this function accepts 0 or 1 argument, you provided %d", len(args))
 			}
 
 			if len(args) > 0 && args[0].Type() != object.STRING_OBJ {
-				return newError(fmt.Sprintf(`Tafadhali tumia alama ya nukuu: "%s"`, args[0].Inspect()))
+				return newError(fmt.Sprintf(`Please use quotes: "%s"`, args[0].Inspect()))
 			}
 			if len(args) == 1 {
 				prompt := args[0].(*object.String).Value
@@ -30,7 +30,7 @@ var builtins = map[string]*object.Builtin{
 
 			line, _, err := buffer.ReadLine()
 			if err != nil && err != io.EOF {
-				return newError("Nimeshindwa kusoma uliyo yajaza")
+				return newError("Failed to read input")
 			}
 
 			return &object.String{Value: string(line)}
@@ -44,7 +44,7 @@ var builtins = map[string]*object.Builtin{
 				var arr []string
 				for _, arg := range args {
 					if arg == nil {
-						return newError("Hauwezi kufanya operesheni hii")
+						return newError("Operation cannot be performed on nil")
 					}
 					arr = append(arr, arg.Inspect())
 				}
@@ -54,7 +54,7 @@ var builtins = map[string]*object.Builtin{
 			return nil
 		},
 	},
-	"_andika": {
+	"write": {
 		Fn: func(args ...object.Object) object.Object {
 			if len(args) == 0 {
 				return &object.String{Value: "\n"}
@@ -62,7 +62,7 @@ var builtins = map[string]*object.Builtin{
 				var arr []string
 				for _, arg := range args {
 					if arg == nil {
-						return newError("Hauwezi kufanya operesheni hii")
+						return newError("Operation cannot be performed on nil")
 					}
 					arr = append(arr, arg.Inspect())
 				}
@@ -71,34 +71,34 @@ var builtins = map[string]*object.Builtin{
 			}
 		},
 	},
-	"aina": {
+	"type": {
 		Fn: func(args ...object.Object) object.Object {
 			if len(args) != 1 {
-				return newError("Samahani, tunahitaji hoja 1, wewe umeweka %d", len(args))
+				return newError("Sorry, this function requires 1 argument, you provided %d", len(args))
 			}
 
 			return &object.String{Value: string(args[0].Type())}
 		},
 	},
-	"fungua": {
+	"open": {
 		Fn: func(args ...object.Object) object.Object {
 
 			if len(args) != 1 {
-				return newError("Samahani, tunahitaji hoja 1, wewe umeweka %d", len(args))
+				return newError("Sorry, this function requires 1 argument, you provided %d", len(args))
 			}
 			filename := args[0].(*object.String).Value
 
 			file, err := os.ReadFile(filename)
 			if err != nil {
-				return &object.Error{Message: "Tumeshindwa kusoma faili au faili halipo"}
+				return &object.Error{Message: "Failed to read file or file does not exist"}
 			}
 			return &object.File{Filename: filename, Content: string(file)}
 		},
 	},
-	"mfululizo": {
+	"range": {
 		Fn: func(args ...object.Object) object.Object {
 			if len(args) < 1 || len(args) > 3 {
-				return newError("Samahani, mfululizo inahitaji hoja 1 hadi 3, wewe umeweka %d", len(args))
+				return newError("Sorry, range requires 1 to 3 arguments, you provided %d", len(args))
 			}
 
 			var start, end, step int64
@@ -108,34 +108,34 @@ var builtins = map[string]*object.Builtin{
 			case 1:
 				end, err = getIntValue(args[0])
 				if err != nil {
-					return newError("Hoja lazima iwe nambari nzima")
+					return newError("Argument must be an integer")
 				}
 				start, step = 0, 1
 			case 2:
 				start, err = getIntValue(args[0])
 				if err != nil {
-					return newError("Hoja ya kwanza lazima iwe nambari nzima")
+					return newError("First argument must be an integer")
 				}
 				end, err = getIntValue(args[1])
 				if err != nil {
-					return newError("Hoja ya pili lazima iwe nambari nzima")
+					return newError("Second argument must be an integer")
 				}
 				step = 1
 			case 3:
 				start, err = getIntValue(args[0])
 				if err != nil {
-					return newError("Hoja ya kwanza lazima iwe nambari nzima")
+					return newError("First argument must be an integer")
 				}
 				end, err = getIntValue(args[1])
 				if err != nil {
-					return newError("Hoja ya pili lazima iwe nambari nzima")
+					return newError("Second argument must be an integer")
 				}
 				step, err = getIntValue(args[2])
 				if err != nil {
-					return newError("Hoja ya tatu lazima iwe nambari nzima")
+					return newError("Third argument must be an integer")
 				}
 				if step == 0 {
-					return newError("Hatua haiwezi kuwa sifuri")
+					return newError("Step cannot be zero")
 				}
 			}
 
@@ -148,74 +148,35 @@ var builtins = map[string]*object.Builtin{
 		},
 	},
 
-	"badilisha": {
+	"convert": {
 		Fn: func(args ...object.Object) object.Object {
 			if len(args) != 2 {
-				return newError("Samahani, badili inahitaji hoja 2, wewe umeweka %d", len(args))
+				return newError("Sorry, convert requires 2 arguments, you provided %d", len(args))
 			}
 
 			value := args[0]
 			targetType := args[1]
 
 			if targetType.Type() != object.STRING_OBJ {
-				return newError("Aina ya lengo lazima iwe neno")
+				return newError("Target type must be a string")
 			}
 
 			targetTypeStr := targetType.(*object.String).Value
 
 			switch targetTypeStr {
-			case "NAMBA":
+			case "INTEGER":
 				return convertToInteger(value)
-			case "DESIMALI":
+			case "FLOAT":
 				return convertToFloat(value)
-			case "NENO":
+			case "STRING":
 				return convertToString(value)
 			case "BOOLEAN":
 				return convertToBoolean(value)
 			default:
-				return newError("Aina isiyojulikana: %s", targetTypeStr)
+				return newError("Unknown type: %s", targetTypeStr)
 			}
 		},
 	},
-
-	// "jumla": {
-	// 	Fn: func(args ...object.Object) object.Object {
-	// 		if len(args) != 1 {
-	// 			return newError("Hoja hazilingani, tunahitaji=1, tumepewa=%d", len(args))
-	// 		}
-
-	// 		switch arg := args[0].(type) {
-	// 		case *object.Array:
-
-	// 			var sums float64
-	// 			for _, num := range arg.Elements {
-
-	// 				if num.Type() != object.INTEGER_OBJ && num.Type() != object.FLOAT_OBJ {
-	// 					return newError("Samahani namba tu zinahitajika")
-	// 				} else {
-	// 					if num.Type() == object.INTEGER_OBJ {
-	// 						no, _ := strconv.Atoi(num.Inspect())
-	// 						floatnum := float64(no)
-	// 						sums += floatnum
-	// 					} else if num.Type() == object.FLOAT_OBJ {
-	// 						no, _ := strconv.ParseFloat(num.Inspect(), 64)
-	// 						sums += no
-	// 					}
-
-	// 				}
-	// 			}
-
-	// 			if math.Mod(sums, 1) == 0 {
-	// 				return &object.Integer{Value: int64(sums)}
-	// 			}
-
-	// 			return &object.Float{Value: float64(sums)}
-
-	// 		default:
-	// 			return newError("Samahani, hii function haitumiki na %s", args[0].Type())
-	// 		}
-	// 	},
-	// },
 }
 
 func getIntValue(obj object.Object) (int64, error) {
@@ -223,6 +184,6 @@ func getIntValue(obj object.Object) (int64, error) {
 	case *object.Integer:
 		return obj.Value, nil
 	default:
-		return 0, fmt.Errorf("expected integer, got %T", obj)
+		return 0, fmt.Errorf("expected Integer, got %s", obj.Type())
 	}
 }
