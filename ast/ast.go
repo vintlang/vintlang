2910ca14225b7c4807921b23f3d5d1883d3cb189
tacheraSasi <tacherasasi@gmail.com>
2024-11-26 00:45:30 +0300

    vintlang website init

--- ast/ast.go
+++ ast/ast.go
@@ -398,7 +398,7 @@ func (we *WhileExpression) TokenLiteral() string { return we.Token.Literal }
 func (we *WhileExpression) String() string {
 	var out bytes.Buffer
 
-	out.WriteString("wakati")
+	out.WriteString("while")
 	out.WriteString(we.Condition.String())
 	out.WriteString(" ")
 	out.WriteString(we.Consequence.String())
