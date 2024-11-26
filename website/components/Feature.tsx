68ca39ad025d7294dd0f19aa22b8d6ac5b0ad212
tacheraSasi <tacherasasi@gmail.com>
2024-11-26 20:17:30 +0300

    AnimatedBackground

--- website/components/Feature.tsx
+++ website/components/Feature.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { ForwardRefExoticComponent, RefAttributes } from "react";
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 import { LucideProps } from "lucide-react";
 
 interface VintLangFeature {
