04dfe1fa208496958191aeba056e6639c5fe63bb
tacheraSasi <tacherasasi@gmail.com>
2024-11-26 20:30:27 +0300

    codeExamples

--- website/components/code-example.tsx
+++ website/components/code-example.tsx
@@ -3,43 +3,7 @@
 import { useEffect, useState } from 'react'
 import { motion } from 'motion/react'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
-
-const codeExamples = {
-  basics: `// Basic VintLang Operations
-let name = "VintLang"
-s = name.split("") 
-for i in s { 
-    print(i)
-}
-
-// Type conversion
-age = "10"
-convert(age, "INTEGER")
-print(type(age))`,
-  
-  functions: `// Function Definition in VintLang
-let printDetails = func(name, age, height) {
-    print("My name is " + name + 
-          ", I am " + age + 
-          " years old, and my height is " + 
-          height + " feet.")
-}
-
-// Function call
-printDetails("VintLang", "10", "6.0")`,
-  
-  timeAndNet: `// Time and Network Operations
-import time
-import net
-
-// Time operations
-print(time.format(time.now(), "02-01-2006 15:04:05"))
-print(time.add(time.now(), "1h"))
-
-// Network request
-let res = net.get("https://example.com")
-print(res)`,
-}
+import { codeExamples } from '@/lib/codeExample'
 
 export default function CodeExample() {
   const [activeTab, setActiveTab] = useState('basics')
