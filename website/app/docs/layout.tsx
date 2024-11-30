2252d5096d6eb136253567ba94ca684ad6652f23
tacheraSasi <tacherasasi@gmail.com>
2024-12-01 00:09:52 +0300

    unyama

--- website/app/docs/layout.tsx
+++ website/app/docs/layout.tsx
@@ -10,14 +10,15 @@ export default function DocsLayout({ children }: DocsLayoutProps) {
   return (
     <div className="flex min-h-screen max-w-screen-xl mx-auto">
       {/* Sidebar */}
-      <aside className="fixed left-0 top-0 z-30 w-[250px] sm:w-[300px] md:w-[380px] h-full border-r bg-background">
+      <aside className="hidden lg:block fixed left-0 top-0 z-30 w-[250px] sm:w-[300px] md:w-[380px] h-full border-r bg-background">
         <ScrollArea className="h-full">
           <Sidebar />
         </ScrollArea>
       </aside>
+
       {/* Main Content */}
       <main className="flex-1 pl-0 sm:pl-[250px] md:pl-[380px]">
-        <SiteHeader/>
+        <SiteHeader />
         <div className="container max-w-3xl py-6 lg:py-10">{children}</div>
       </main>
     </div>
