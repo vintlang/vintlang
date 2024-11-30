384ae595a466cda42844606338a0c1cc307301c4
tacheraSasi <tacherasasi@gmail.com>
2024-11-30 21:31:02 +0300

    installation docs

--- website/app/docs/page.tsx
+++ website/app/docs/page.tsx
@@ -1,22 +1,24 @@
-import { Metadata } from "next"
-import { DocsHeader } from "@/components/docs/docs-header"
-import { Features } from "@/components/docs/features"
-import { Core } from "@/components/docs/core"
-import { Introduction } from "@/components/docs/Introduction"
+import { Metadata } from "next";
+import { DocsHeader } from "@/components/docs/docs-header";
+import { Features } from "@/components/docs/features";
+import { Core } from "@/components/docs/core";
+import { Introduction } from "@/components/docs/Introduction";
+import Installation from "@/components/docs/Installation";
 
 export const metadata: Metadata = {
   title: "Documentation - VintLang",
-  description: "Documentation for VintLang - A modern programming language built with Go",
-}
+  description:
+    "Documentation for VintLang - A modern programming language built with Go",
+};
 
 export default function DocsPage() {
   return (
     <div className="space-y-10">
-      <Introduction/>
+      <Introduction />
+      <Installation/>
       <DocsHeader />
       <Features />
       <Core />
     </div>
-  )
+  );
 }
-
--- website/components/docs/Installation.tsx
+++ website/components/docs/Installation.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import SectionHeader from './SectionHeader'
+import MarkdownRenderer from '../MarkdownRender'
+import { getMarkdownContent } from '@/lib/utils'
 
 const Installation = () => {
+    const markdown = getMarkdownContent("README.md")
+
   return (
     <div className="" id='installation'>
         <SectionHeader title="Installation"/>
+
+        <MarkdownRenderer markdown={markdown}/>
+
     </div>
   )
 }
