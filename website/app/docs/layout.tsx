08d35ba8a39cf87f0483e6d712f2e75be0055cd4
tacheraSasi <tacherasasi@gmail.com>
2024-11-30 23:59:16 +0300

    responsive

--- website/app/docs/layout.tsx
+++ website/app/docs/layout.tsx
@@ -1,24 +1,25 @@
-import { Sidebar } from "@/components/docs/sidebar"
-import { ScrollArea } from "@/components/ui/scroll-area"
+import { Sidebar } from "@/components/docs/sidebar";
+import { SiteHeader } from "@/components/docs/site-header";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 interface DocsLayoutProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export default function DocsLayout({ children }: DocsLayoutProps) {
   return (
     <div className="flex min-h-screen max-w-screen-xl mx-auto">
-      <aside className="fixed left-0 top-0 z-30 h-screen w-[380px] border-r bg-background">
+      {/* Sidebar */}
+      <aside className="fixed left-0 top-0 z-30 w-[250px] sm:w-[300px] md:w-[380px] h-full border-r bg-background">
         <ScrollArea className="h-full">
           <Sidebar />
         </ScrollArea>
       </aside>
-      <main className="flex-1 pl-[250px]">
-        <div className="container max-w-3xl py-6 lg:py-10">
-          {children}
-        </div>
+      {/* Main Content */}
+      <main className="flex-1 pl-0 sm:pl-[250px] md:pl-[380px]">
+        <SiteHeader/>
+        <div className="container max-w-3xl py-6 lg:py-10">{children}</div>
       </main>
     </div>
-  )
+  );
 }
-
