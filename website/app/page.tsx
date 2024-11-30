6183f12786c987f6dc6a6f123ea00a2d3dd6bf86
tacheraSasi <tacherasasi@gmail.com>
2024-11-30 19:29:07 +0300

    loading skeleton

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,10 +1,14 @@
 import { Suspense } from 'react'
 import { LandingPage } from '@/components/landing-page'
+import LoadingSkeleton from '@/components/LoadingSkeleton'
 
 export default function Home() {
   return (
     
+    <Suspense fallback={<LoadingSkeleton/>}>
       <LandingPage />
+    </Suspense>
+      
    
   )
 }
