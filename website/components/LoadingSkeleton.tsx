7717168e3740416a9f94849af3d0d74827e82051
tacheraSasi <tacherasasi@gmail.com>
2024-11-30 19:29:07 +0300

    loading skeleton

---
+++ website/components/LoadingSkeleton.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingSkeleton = () => {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default LoadingSkeleton;
