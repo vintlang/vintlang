7fd6f7636e5a5ba5047ba05460321f07d372427b
tacheraSasi <tacherasasi@gmail.com>
2024-11-30 23:12:59 +0300

    Finally rendered the markdown straight from github

--- website/components/docs/Installation.tsx
+++ website/components/docs/Installation.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import SectionHeader from './SectionHeader'
-import MarkdownRenderer from '../MarkdownRender'
-import { getMarkdownContent } from '@/lib/utils'
+import React from "react";
+import { fetchMarkdown, getMarkdownContent } from "@/lib/utils";
+import SectionHeader from "./SectionHeader";
+// import MarkdownRenderer from "../MarkdownRender";
+import { Markdown } from "../Markdown";
 
-const Installation = () => {
-    const markdown = getMarkdownContent("README.md")
+export const revalidate = 60; // Revalidate every 60 seconds (ISR)
+
+export default async function InstallationPage() {
+  const markdown = await fetchMarkdown("README.md");
 
   return (
-    <div className="" id='installation'>
-        <SectionHeader title="Installation"/>
-
-        <MarkdownRenderer markdown={markdown}/>
-
+    <div className="p-6" id="installation">
+      <SectionHeader title="Installation" />
+      {/* <MarkdownRenderer markdown={markdown} /> */}
+      <Markdown>{markdown}</Markdown>
     </div>
-  )
+  );
 }
-
-export default Installation
