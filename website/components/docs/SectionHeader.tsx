8da48ab5914952a36e02d58c828f0c2a5775be08
Revaycolizer <albinuslucas@gmail.com>
2024-12-01 11:48:33 +0300

    New Changes

--- website/components/docs/SectionHeader.tsx
+++ website/components/docs/SectionHeader.tsx
@@ -6,7 +6,7 @@ interface SectionHeaderProps{
 
 const SectionHeader = ({title}:SectionHeaderProps) => {
   return (
-    <h1 className="text-6xl font-bold tracking-tight dark:text-neutral-300">
+    <h1 className="text-base md:text-6xl font-bold tracking-tight dark:text-neutral-300">
       {title}
     </h1>
   );
--- website/components/docs/core.tsx
+++ website/components/docs/core.tsx
@@ -1,10 +1,10 @@
 export function Core() {
     return (
       <div className="space-y-4">
-        <h2 className="text-2xl font-semibold tracking-tight">Core</h2>
-        <p className="text-muted-foreground">
+        <h2 className="text-base md:text-2xl font-semibold tracking-tight">Core</h2>
+        <p className="text-xs md:text-base text-muted-foreground">
           VintLang has a very minimal API, exposing only a few exports from the main{" "}
-          <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm">
+          <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-[9x] md:text-sm">
             vintlang
           </code>{" "}
           bundle. They are split into four categories below.
