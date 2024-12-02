7e6800f6b67e9cbb01797c0e1c6e166362e113f8
tacheraSasi <tacherasasi@gmail.com>
2024-12-02 22:57:23 +0300

    more more fixes

--- website/components/docs/site-header.tsx
+++ website/components/docs/site-header.tsx
@@ -9,7 +9,7 @@ import { ModeToggle } from "@/components/docs/mode-toggle"
 
 export function SiteHeader() {
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 items-center">
         {/* <MainNav /> */}
         <MobileNav />
