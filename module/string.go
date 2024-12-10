deec2379f58039242f8cb0ba1fa2ae7d76ce91f7
tacheraSasi <tacherasasi@gmail.com>
2024-12-10 17:08:57 +0300

    created a slug method to the strings module

--- module/string.go
+++ module/string.go
@@ -1,7 +1,9 @@
 package module
 
 import (
+	"regexp"
 	"strings"
+
 	"github.com/ekilie/vint-lang/object"
 	"github.com/xrash/smetrics" // A library for string metrics, like Levenshtein
 )
@@ -20,7 +22,37 @@ func init() {
 	StringFunctions["length"] = length
 	StringFunctions["indexOf"] = indexOf
 	StringFunctions["similarity"] = similarity 
+	StringFunctions["slug"] = slug 
 }
+
+// Creates a slug string from a normal string
+func slug(args []object.Object, defs map[string]object.Object) object.Object {
+	// Ensure exactly one argument is passed
+	if len(args) != 1 {
+		return &object.Error{Message: "string.slug requires exactly one argument"}
+	}
+
+	// Inspect the argument and convert it to a string
+	input := args[0].Inspect()
+
+	// Convert the input to lowercase
+	input = strings.ToLower(input)
+
+	// Remove all non-alphanumeric characters except spaces and hyphens
+	re := regexp.MustCompile(`[^a-z0-9\s-]+`)
+	input = re.ReplaceAllString(input, "")
+
+	// Replace spaces and multiple hyphens with a single hyphen
+	re = regexp.MustCompile(`[\s-]+`)
+	input = re.ReplaceAllString(input, "-")
+
+	// Trim leading and trailing hyphens
+	input = strings.Trim(input, "-")
+
+	// Return the result as a Vint object string
+	return &object.String{Value: input}
+}
+
 
 // similarity computes a similarity score between two strings
 func similarity(args []object.Object, defs map[string]object.Object) object.Object {
