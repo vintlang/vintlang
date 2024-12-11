e623cdb5170730aa18347b071ddbe250611ad42c
tacheraSasi <tacherasasi@gmail.com>
2024-12-12 02:02:24 +0300

    update init

--- toolkit/toolkit.go
+++ toolkit/toolkit.go
@@ -1,7 +1,13 @@
 package toolkit
 
+import "fmt"
+
 var CLI_ARGS []string = []string{}
 
 func GetCliArgs()[]string{// Returns the CLI_ARGS 
 	return CLI_ARGS
+}
+
+func Update(){
+	fmt.Println("updating vint...")
 }
