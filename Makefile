06aea5c3c331a8fea9894c79f6c72f6cae6ed50e
tacheraSasi <tacherasasi@gmail.com>
2025-01-07 15:44:27 +0300

    upped the verison to 0.1.4 also modified the toolkit package

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=0.1.3
+VERSION=0.1.4
 
 build:
 	make build_android
--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,15 +1,11 @@
-import { Suspense } from 'react'
-import { LandingPage } from '@/components/landing-page'
-import LoadingSkeleton from '@/components/LoadingSkeleton'
+import { Suspense } from "react";
+import { LandingPage } from "@/components/landing-page";
+import LoadingSkeleton from "@/components/LoadingSkeleton";
 
 export default function Home() {
   return (
-    
-    <Suspense fallback={<LoadingSkeleton/>}>
+    <Suspense fallback={<LoadingSkeleton />}>
       <LandingPage />
     </Suspense>
-      
-   
-  )
+  );
 }
-
