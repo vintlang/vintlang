0f37f607939f3d37a1add837c4aaf8b08eee1952
tacheraSasi <tacherasasi@gmail.com>
2025-05-06 13:57:22 +0300

    chore: update dependencies in bun.lock and refactor sidebar component to use external navigation

---
+++ website/lib/navigation.ts
@@ -0,0 +1,29 @@
+import { Hand, ArrowDownToLine, FileText, BookOpen, HelpCircle } from "lucide-react";
+
+export const navigation = [
+    {
+      name: "Introduction",
+      href: "#",
+      icon: Hand,
+    },
+    {
+      name: "Installation",
+      href: "#installation",
+      icon: ArrowDownToLine,
+    },
+    {
+      name: "Documentation",
+      href: "#docs",
+      icon: FileText,
+    },
+    {
+      name: "Tutorial",
+      href: "#tutorial",
+      icon: BookOpen,
+    },
+    {
+      name: "Sponsor",
+      href: "#sponsor",
+      icon: HelpCircle,
+    },
+  ];
