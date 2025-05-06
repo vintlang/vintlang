dfd0063120269704a5f9d3948c9660ca7943a305
tacheraSasi <tacherasasi@gmail.com>
2025-05-06 14:05:35 +0300

    feat: update Bundler documentation page to include Introduction component and modify section header title

--- website/app/docs/bundler/page.tsx
+++ website/app/docs/bundler/page.tsx
@@ -1,3 +1,10 @@
-export default function Page() {
+import { Introduction } from "@/components/docs/Introduction";
+
+export default function BundlerPage() {
+    return (
+        <div className="space-y-10">
+            <Introduction />
+        </div>
+    )
 
 }
