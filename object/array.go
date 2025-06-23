d31eb37d0cc8582bf692a9218f130c16b7b3128e
tacherasasi <tacherasasi@gmail.com>
2025-06-23 21:35:05 +0300

    feat: add array manipulation methods including pop, shift, unshift, reverse, and sort with corresponding documentation updates

--- object/array.go
+++ object/array.go
@@ -2,6 +2,7 @@ package object
 
 import (
 	"bytes"
+	"sort"
 	"strings"
 )
 
@@ -57,6 +58,18 @@ func (a *Array) Method(method string, args []Object) Object {
 		return a.filter(args)
 	case "find":
 		return a.find(args)
+	case "pop":
+		return a.pop()
+	case "shift":
+		return a.shift()
+	case "unshift":
+		return a.unshift(args)
+	case "reverse":
+		return a.reverse()
+	case "sort":
+		return a.sort()
+	case "map":
+		return a.mapMethod(args)
 	default:
 		return newError("Sorry, the method '%s' is not supported for this object.", method)
 	}
@@ -128,4 +141,102 @@ func (a *Array) find(args []Object) Object {
 		}
 	}
 	return &Null{}
+}
+
+func (a *Array) pop() Object {
+	if len(a.Elements) == 0 {
+		return &Null{}
+	}
+	last := a.Elements[len(a.Elements)-1]
+	a.Elements = a.Elements[:len(a.Elements)-1]
+	return last
+}
+
+func (a *Array) shift() Object {
+	if len(a.Elements) == 0 {
+		return &Null{}
+	}
+	first := a.Elements[0]
+	a.Elements = a.Elements[1:]
+	return first
+}
+
+func (a *Array) unshift(args []Object) Object {
+	if len(args) == 0 {
+		return newError("unshift() expects at least 1 argument, got 0")
+	}
+	a.Elements = append(args, a.Elements...)
+	return a
+}
+
+func (a *Array) reverse() Object {
+	length := len(a.Elements)
+	for i := 0; i < length/2; i++ {
+		a.Elements[i], a.Elements[length-1-i] = a.Elements[length-1-i], a.Elements[i]
+	}
+	return a
+}
+
+func (a *Array) sort() Object {
+	// Only sorts arrays of Integers or Strings for simplicity
+	if len(a.Elements) == 0 {
+		return a
+	}
+	firstType := a.Elements[0].Type()
+	switch firstType {
+	case INTEGER_OBJ:
+		ints := make([]int, len(a.Elements))
+		for i, el := range a.Elements {
+			intEl, ok := el.(*Integer)
+			if !ok {
+				return newError("sort() only supports arrays of integers or strings")
+			}
+			ints[i] = int(intEl.Value)
+		}
+		sort.Ints(ints)
+		for i, v := range ints {
+			a.Elements[i] = &Integer{Value: int64(v)}
+		}
+	case STRING_OBJ:
+		strs := make([]string, len(a.Elements))
+		for i, el := range a.Elements {
+			strEl, ok := el.(*String)
+			if !ok {
+				return newError("sort() only supports arrays of integers or strings")
+			}
+			strs[i] = strEl.Value
+		}
+		sort.Strings(strs)
+		for i, v := range strs {
+			a.Elements[i] = &String{Value: v}
+		}
+	default:
+		return newError("sort() only supports arrays of integers or strings")
+	}
+	return a
+}
+
+func (a *Array) mapMethod(args []Object) Object {
+	if len(args) != 1 {
+		return newError("map() expects exactly 1 argument, got %d", len(args))
+	}
+	fn, ok := args[0].(*Function)
+	if !ok {
+		return newError("map() expects a function as its argument")
+	}
+	mapped := make([]Object, len(a.Elements))
+	for i, el := range a.Elements {
+		// For simplicity, only pass the element as argument
+		callObj, found := fn.Env.Get("__call__")
+		if !found {
+			return newError("map() function does not have a __call__ method")
+		}
+		builtin, ok := callObj.(*Builtin)
+		if !ok {
+			return newError("map() function's __call__ is not a builtin function")
+		}
+		result := builtin.Fn(el)
+		mapped[i] = result
+	}
+	return &Array{Elements: mapped}
 }
