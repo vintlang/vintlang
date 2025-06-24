ba7fbc738f85365d9e9f22a4570aa6e733222e90
tacherasasi <tacherasasi@gmail.com>
2025-06-24 12:58:45 +0300

    feat: implement automatic execution of init function in package evaluation to enhance initialization process

--- evaluator/package.go
+++ evaluator/package.go
@@ -13,6 +13,17 @@ func evalPackage(node *ast.Package, env *object.Environment) object.Object {
 	}
 
 	Eval(node.Block, Package.Scope)
+
+	// Automatically runs the init function if it exists
+	if initFunc, ok := Package.Scope.Get("init"); ok {
+		if fn, ok := initFunc.(*object.Function); ok {
+			// Call the init function
+			// We can create a new environment for the function call if needed
+			// but for an init function, it should probably run in the package's scope
+			extendedEnv := object.NewEnclosedEnvironment(Package.Scope)
+			Eval(fn.Body, extendedEnv)
+		}
+	}
 	env.Define(node.Name.Value, Package)
 	return Package
 }
