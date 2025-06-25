bebbf00f60f3dfb1ff119f7dc8a2bc1b76826c35
tacheraSasi <tacherasasi@gmail.com>
2025-06-25 12:22:10 +0300

    feat(compiler, vm): implement arithmetic and boolean operations with corresponding tests

--- compiler/compiler_test.go
+++ compiler/compiler_test.go
@@ -1,11 +1,13 @@
 package compiler
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/vintlang/vintlang/ast"
 	"github.com/vintlang/vintlang/code"
 	"github.com/vintlang/vintlang/lexer"
+	"github.com/vintlang/vintlang/object"
 	"github.com/vintlang/vintlang/parser"
 )
 
@@ -16,7 +18,158 @@ type compilerTestCase struct {
 }
 
 func TestIntegerArithmetic(t *testing.T) {
-	// We will add test cases here later
+	tests := []compilerTestCase{
+		{
+			input:             "1; 2",
+			expectedConstants: []interface{}{1, 2},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 0),
+				code.Make(code.OpPop),
+				code.Make(code.OpConstant, 1),
+				code.Make(code.OpPop),
+			},
+		},
+	}
+
+	runCompilerTests(t, tests)
+}
+
+func TestBooleanExpressions(t *testing.T) {
+	tests := []compilerTestCase{
+		{
+			input:             "true",
+			expectedConstants: []interface{}{},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpTrue),
+				code.Make(code.OpPop),
+			},
+		},
+		{
+			input:             "false",
+			expectedConstants: []interface{}{},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpFalse),
+				code.Make(code.OpPop),
+			},
+		},
+	}
+
+	runCompilerTests(t, tests)
+}
+
+func runCompilerTests(t *testing.T, tests []compilerTestCase) {
+	t.Helper()
+
+	for _, tt := range tests {
+		program := parse(tt.input)
+
+		compiler := New()
+		err := compiler.Compile(program)
+		if err != nil {
+			t.Fatalf("compiler error: %s", err)
+		}
+
+		bytecode := compiler.Bytecode()
+
+		err = testInstructions(tt.expectedInstructions, bytecode.Instructions)
+		if err != nil {
+			t.Fatalf("testInstructions failed: %s", err)
+		}
+
+		err = testConstants(t, tt.expectedConstants, bytecode.Constants)
+		if err != nil {
+			t.Fatalf("testConstants failed: %s", err)
+		}
+	}
+}
+
+func testInstructions(
+	expected []code.Instructions,
+	actual code.Instructions,
+) error {
+	concatted := concatInstructions(expected)
+
+	if len(actual) != len(concatted) {
+		return fmt.Errorf("wrong instructions length.\nwant=%q\ngot =%q",
+			concatted, actual)
+	}
+
+	for i, ins := range concatted {
+		if actual[i] != ins {
+			return fmt.Errorf("wrong instruction at %d.\nwant=%q\ngot =%q",
+				i, concatted, actual)
+		}
+	}
+
+	return nil
+}
+
+func concatInstructions(s []code.Instructions) code.Instructions {
+	out := code.Instructions{}
+	for _, ins := range s {
+		out = append(out, ins...)
+	}
+	return out
+}
+
+func testConstants(
+	t *testing.T,
+	expected []interface{},
+	actual []object.Object,
+) error {
+	if len(expected) != len(actual) {
+		return fmt.Errorf("wrong number of constants. got=%d, want=%d",
+			len(actual), len(expected))
+	}
+
+	for i, constant := range expected {
+		switch constant := constant.(type) {
+		case int:
+			err := testIntegerObject(int64(constant), actual[i])
+			if err != nil {
+				return fmt.Errorf("constant %d - testIntegerObject failed: %s",
+					i, err)
+			}
+		case bool:
+			err := testBooleanObject(constant, actual[i])
+			if err != nil {
+				return fmt.Errorf("constant %d - testBooleanObject failed: %s",
+					i, err)
+			}
+		}
+	}
+
+	return nil
+}
+
+func testIntegerObject(expected int64, actual object.Object) error {
+	result, ok := actual.(*object.Integer)
+	if !ok {
+		return fmt.Errorf("object is not Integer. got=%T (%+v)",
+			actual, actual)
+	}
+
+	if result.Value != expected {
+		return fmt.Errorf("object has wrong value. got=%d, want=%d",
+			result.Value, expected)
+	}
+
+	return nil
+}
+
+func testBooleanObject(expected bool, actual object.Object) error {
+	result, ok := actual.(*object.Boolean)
+	if !ok {
+		return fmt.Errorf("object is not Boolean. got=%T (%+v)",
+			actual, actual)
+	}
+
+	if result.Value != expected {
+		return fmt.Errorf("object has wrong value. got=%t, want=%t",
+			result.Value, expected)
+	}
+
+	return nil
 }
 
 func parse(input string) *ast.Program {
--- vm/vm.go
+++ vm/vm.go
@@ -1,18 +1,28 @@
 package vm
 
 import (
+	"encoding/binary"
+	"fmt"
+
+	"github.com/vintlang/vintlang/code"
 	"github.com/vintlang/vintlang/compiler"
 	"github.com/vintlang/vintlang/object"
 )
 
 const StackSize = 2048
 
+var (
+	True  = &object.Boolean{Value: true}
+	False = &object.Boolean{Value: false}
+)
+
 type VM struct {
 	constants    []object.Object
 	instructions []byte
 
-	stack []object.Object
-	sp    int // Always points to the next value. Top of the stack is stack[sp-1]
+	stack               []object.Object
+	sp                  int // Always points to the next value. Top of the stack is stack[sp-1]
+	lastPoppedStackElem object.Object
 }
 
 func New(bytecode *compiler.Bytecode) *VM {
@@ -24,6 +34,10 @@ func New(bytecode *compiler.Bytecode) *VM {
 	}
 }
 
+func (vm *VM) LastPoppedStackElem() object.Object {
+	return vm.lastPoppedStackElem
+}
+
 func (vm *VM) StackTop() object.Object {
 	if vm.sp == 0 {
 		return nil
@@ -32,6 +46,82 @@ func (vm *VM) StackTop() object.Object {
 }
 
 func (vm *VM) Run() error {
-	// the main fetch-decode-execute cycle will go here
+	for ip := 0; ip < len(vm.instructions); ip++ {
+		op := vm.instructions[ip]
+
+		switch op {
+		case byte(code.OpConstant):
+			constIndex := binary.BigEndian.Uint16(vm.instructions[ip+1:])
+			ip += 2 // Move instruction pointer past the 2-byte operand
+
+			err := vm.push(vm.constants[constIndex])
+			if err != nil {
+				return err
+			}
+		case byte(code.OpPop):
+			vm.pop()
+		case byte(code.OpAdd), byte(code.OpSub), byte(code.OpMul), byte(code.OpDiv):
+			err := vm.executeBinaryOperation(op)
+			if err != nil {
+				return err
+			}
+		case byte(code.OpTrue):
+			err := vm.push(True)
+			if err != nil {
+				return err
+			}
+		case byte(code.OpFalse):
+			err := vm.push(False)
+			if err != nil {
+				return err
+			}
+		}
+	}
 	return nil
+}
+
+func (vm *VM) executeBinaryOperation(op byte) error {
+	right := vm.pop()
+	left := vm.pop()
+
+	leftVal := left.(*object.Integer).Value
+	rightVal := right.(*object.Integer).Value
+
+	var result int64
+
+	switch op {
+	case byte(code.OpAdd):
+		result = leftVal + rightVal
+	case byte(code.OpSub):
+		result = leftVal - rightVal
+	case byte(code.OpMul):
+		result = leftVal * rightVal
+	case byte(code.OpDiv):
+		result = leftVal / rightVal
+	default:
+		return fmt.Errorf("unknown integer operator: %d", op)
+	}
+
+	return vm.push(&object.Integer{Value: result})
+}
+
+func (vm *VM) push(o object.Object) error {
+	if vm.sp >= StackSize {
+		return fmt.Errorf("stack overflow")
+	}
+
+	vm.stack[vm.sp] = o
+	vm.sp++
+
+	return nil
+}
+
+func (vm *VM) pop() object.Object {
+	if vm.sp == 0 {
+		return nil
+	}
+	o := vm.stack[vm.sp-1]
+	vm.sp--
+	vm.lastPoppedStackElem = o
+	return o
 }
