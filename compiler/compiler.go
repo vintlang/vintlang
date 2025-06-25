3ea0f1044583524df8fa690caf050b681b4feeb2
tacherasasi <tacherasasi@gmail.com>
2025-06-25 12:34:12 +0300

    feat(compiler, vm): add comparison operators and corresponding tests for boolean expressions

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -62,6 +62,22 @@ func (c *Compiler) Compile(node ast.Node) error {
 			c.emit(code.OpMul)
 		case "/":
 			c.emit(code.OpDiv)
+		case ">":
+			c.emit(code.OpGreaterThan)
+		case "<":
+			err := c.Compile(node.Right)
+			if err != nil {
+				return err
+			}
+			err = c.Compile(node.Left)
+			if err != nil {
+				return err
+			}
+			c.emit(code.OpGreaterThan)
+		case "==":
+			c.emit(code.OpEqual)
+		case "!=":
+			c.emit(code.OpNotEqual)
 		default:
 			return fmt.Errorf("unknown operator: %s", node.Operator)
 		}
