66b625ac6f1ef5bc74acd1122d912d8e02c8999d
tacherasasi <tacherasasi@gmail.com>
2025-06-25 12:22:10 +0300

    feat(compiler, vm): implement arithmetic and boolean operations with corresponding tests

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -1,6 +1,8 @@
 package compiler
 
 import (
+	"fmt"
+
 	"github.com/vintlang/vintlang/ast"
 	"github.com/vintlang/vintlang/code"
 	"github.com/vintlang/vintlang/object"
@@ -24,7 +26,59 @@ func New() *Compiler {
 }
 
 func (c *Compiler) Compile(node ast.Node) error {
-	// a big switch statement will go here
+	switch node := node.(type) {
+	case *ast.Program:
+		for _, s := range node.Statements {
+			err := c.Compile(s)
+			if err != nil {
+				return err
+			}
+		}
+
+	case *ast.ExpressionStatement:
+		err := c.Compile(node.Expression)
+		if err != nil {
+			return err
+		}
+		c.emit(code.OpPop)
+
+	case *ast.InfixExpression:
+		err := c.Compile(node.Left)
+		if err != nil {
+			return err
+		}
+
+		err = c.Compile(node.Right)
+		if err != nil {
+			return err
+		}
+
+		switch node.Operator {
+		case "+":
+			c.emit(code.OpAdd)
+		case "-":
+			c.emit(code.OpSub)
+		case "*":
+			c.emit(code.OpMul)
+		case "/":
+			c.emit(code.OpDiv)
+		default:
+			return fmt.Errorf("unknown operator: %s", node.Operator)
+		}
+
+	case *ast.Boolean:
+		if node.Value {
+			c.emit(code.OpTrue)
+		} else {
+			c.emit(code.OpFalse)
+		}
+
+	case *ast.IntegerLiteral:
+		integer := &object.Integer{Value: node.Value}
+		c.emit(code.OpConstant, c.addConstant(integer))
+
+	}
+
 	return nil
 }
 
@@ -33,4 +87,16 @@ func (c *Compiler) Bytecode() *Bytecode {
 		Instructions: c.instructions,
 		Constants:    c.constants,
 	}
+}
+
+func (c *Compiler) addConstant(obj object.Object) int {
+	c.constants = append(c.constants, obj)
+	return len(c.constants) - 1
+}
+
+func (c *Compiler) emit(op code.Opcode, operands ...int) int {
+	ins := code.Make(op, operands...)
+	pos := len(c.instructions)
+	c.instructions = append(c.instructions, ins...)
+	return pos
 }
--- vm/vm_test.go
+++ vm/vm_test.go
@@ -1,10 +1,13 @@
 package vm
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/vintlang/vintlang/ast"
+	"github.com/vintlang/vintlang/compiler"
 	"github.com/vintlang/vintlang/lexer"
+	"github.com/vintlang/vintlang/object"
 	"github.com/vintlang/vintlang/parser"
 )
 
@@ -14,7 +17,100 @@ type vmTestCase struct {
 }
 
 func TestIntegerArithmetic(t *testing.T) {
-	// We will add test cases here later
+	tests := []vmTestCase{
+		{"1", 1},
+		{"2", 2},
+		{"1 + 2", 3},
+		{"1 - 2", -1},
+		{"2 * 3", 6},
+		{"4 / 2", 2},
+		{"50 / 2 * 2 + 10 - 5", 55},
+	}
+
+	runVmTests(t, tests)
+}
+
+func TestBooleanExpressions(t *testing.T) {
+	tests := []vmTestCase{
+		{"true", true},
+		{"false", false},
+	}
+
+	runVmTests(t, tests)
+}
+
+func runVmTests(t *testing.T, tests []vmTestCase) {
+	t.Helper()
+
+	for _, tt := range tests {
+		program := parse(tt.input)
+		comp := compiler.New()
+		err := comp.Compile(program)
+		if err != nil {
+			t.Fatalf("compiler error: %s", err)
+		}
+
+		vm := New(comp.Bytecode())
+		err = vm.Run()
+		if err != nil {
+			t.Fatalf("vm error: %s", err)
+		}
+
+		stackElem := vm.LastPoppedStackElem()
+
+		testExpectedObject(t, tt.expected, stackElem)
+	}
+}
+
+func testExpectedObject(
+	t *testing.T,
+	expected interface{},
+	actual object.Object,
+) {
+	t.Helper()
+
+	switch expected := expected.(type) {
+	case int:
+		err := testIntegerObject(int64(expected), actual)
+		if err != nil {
+			t.Errorf("testIntegerObject failed: %s", err)
+		}
+	case bool:
+		err := testBooleanObject(expected, actual)
+		if err != nil {
+			t.Errorf("testBooleanObject failed: %s", err)
+		}
+	}
+}
+
+func testIntegerObject(expected int64, actual object.Object) error {
+	result, ok := actual.(*object.Integer)
+	if !ok {
+		return fmt.Errorf("object is not Integer. got=%T (%+v)",
+			actual, actual)
+	}
+
+	if result.Value != expected {
+		return fmt.Errorf("object has wrong value. got=%d, want=%d",
+			result.Value, expected)
+	}
+
+	return nil
+}
+
+func testBooleanObject(expected bool, actual object.Object) error {
+	result, ok := actual.(*object.Boolean)
+	if !ok {
+		return fmt.Errorf("object is not Boolean. got=%T (%+v)",
+			actual, actual)
+	}
+
+	if result.Value != expected {
+		return fmt.Errorf("object has wrong value. got=%t, want=%t",
+			result.Value, expected)
+	}
+
+	return nil
 }
 
 func parse(input string) *ast.Program {
