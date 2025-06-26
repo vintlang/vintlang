d91ebeec8bf1ff645f51915de09804fa74412682
tacheraSasi <tacherasasi@gmail.com>
2025-06-26 14:10:56 +0300

    refactor(test_vm): comment out main function and related code for testing purposes

--- test_vm.go
+++ test_vm.go
@@ -1,39 +1,39 @@
 package main
 
-import (
-	"fmt"
+// import (
+// 	"fmt"
 
-	"github.com/vintlang/vintlang/compiler"
-	"github.com/vintlang/vintlang/lexer"
-	"github.com/vintlang/vintlang/parser"
-	"github.com/vintlang/vintlang/vm"
-)
+// 	"github.com/vintlang/vintlang/compiler"
+// 	"github.com/vintlang/vintlang/lexer"
+// 	"github.com/vintlang/vintlang/parser"
+// 	"github.com/vintlang/vintlang/vm"
+// )
 
-func main() {
-	// --- Feel free to change the input string to test different expressions! ---
-	input := `(50 / 2 * 2 + 10 - 5) > 20`
+// func main() {
+// 	// --- Feel free to change the input string to test different expressions! ---
+// 	input := `(50 / 2 * 2 + 10 - 5) > 20`
 
-	l := lexer.New(input)
-	p := parser.New(l)
-	program := p.ParseProgram()
+// 	l := lexer.New(input)
+// 	p := parser.New(l)
+// 	program := p.ParseProgram()
 
-	comp := compiler.New()
-	err := comp.Compile(program)
-	if err != nil {
-		fmt.Printf("compiler error: %s\n", err)
-		return
-	}
+// 	comp := compiler.New()
+// 	err := comp.Compile(program)
+// 	if err != nil {
+// 		fmt.Printf("compiler error: %s\n", err)
+// 		return
+// 	}
 
-	machine := vm.New(comp.Bytecode())
-	err = machine.Run()
-	if err != nil {
-		fmt.Printf("vm error: %s\n", err)
-		return
-	}
+// 	machine := vm.New(comp.Bytecode())
+// 	err = machine.Run()
+// 	if err != nil {
+// 		fmt.Printf("vm error: %s\n", err)
+// 		return
+// 	}
 
-	// The result of the last expression is left on the stack.
-	result := machine.LastPoppedStackElem()
-	if result != nil {
-		fmt.Printf("Result of '%s': %s\n", input, result.Inspect())
-	}
-}
+// 	// The result of the last expression is left on the stack.
+// 	result := machine.LastPoppedStackElem()
+// 	if result != nil {
+// 		fmt.Printf("Result of '%s': %s\n", input, result.Inspect())
+// 	}
+// }
