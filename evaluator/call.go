1080dc3d8324137bd208f56becf6da43f0590952
tacheraSasi <tacherasasi@gmail.com>
2025-06-27 11:53:48 +0300

    feat: Enhance evalCall function to support default parameters in function overloads

--- evaluator/call.go
+++ evaluator/call.go
@@ -27,10 +27,27 @@ func evalCall(node *ast.CallExpression, env *object.Environment) object.Object {
 		if len(overloads) > 0 {
 			matched := false
 			for _, fn := range overloads {
-				if len(fn.Parameters) == len(node.Arguments) {
+				paramCount := len(fn.Parameters)
+				argCount := len(node.Arguments)
+				// Allow match if all missing params have defaults
+				if argCount == paramCount {
 					function = fn
 					matched = true
 					break
+				} else if argCount < paramCount {
+					missing := paramCount - argCount
+					missingWithDefaults := 0
+					for i := argCount; i < paramCount; i++ {
+						paramName := fn.Parameters[i].Value
+						if _, ok := fn.Defaults[paramName]; ok {
+							missingWithDefaults++
+						}
+					}
+					if missingWithDefaults == missing {
+						function = fn
+						matched = true
+						break
+					}
 				}
 			}
 			if !matched {
