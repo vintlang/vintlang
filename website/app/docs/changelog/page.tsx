731d0c7ef85dffced64ea277c6c1c75f365f4580
tacheraSasi <tacherasasi@gmail.com>
2025-06-27 12:14:45 +0300

    chore: Update dependencies and add Changelog link to navigation
    - Updated @radix-ui/react-dialog, @radix-ui/react-scroll-area, @radix-ui/react-slot, and @radix-ui/react-tabs to their latest versions.
    - Updated TypeScript and PostCSS dependencies.
    - Added a new "Changelog" entry in the navigation menu.

---
+++ website/app/docs/changelog/page.tsx
@@ -0,0 +1,13 @@
+import Bundler from "@/components/docs/bundler";
+
+
+
+export default function BundlerPage() {
+    
+    return (
+        <div className="space-y-10">
+            <Bundler />
+        </div>
+    )
+
+}
