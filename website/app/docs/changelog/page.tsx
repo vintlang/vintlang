03ab9752c151b6362e7848922d0cf88ef1f6cbc4
tacheraSasi <tacherasasi@gmail.com>
2025-06-27 12:28:41 +0300

    feat: Add metadata for Bundler and Changelog pages in documentation

--- website/app/docs/changelog/page.tsx
+++ website/app/docs/changelog/page.tsx
@@ -1,9 +1,13 @@
 import Bundler from "@/components/docs/bundler";
 import Changelog from "@/components/docs/changelog";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Changelog - VintLang",
+  description: "VintLang Changelog",
+};
 
-
-export default function BundlerPage() {
+export default function ChangelogPage() {
     
     return (
         <div className="space-y-10">
