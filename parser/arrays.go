451757c27bf50bb29314a1aecf868de800dc6d20
tacherasasi <tacherasasi@gmail.com>
2025-07-30 15:03:30 +0300

    removed vintFlow from the codebase since its something that will probably never be implemented or worked up on

--- parser/arrays.go
+++ parser/arrays.go
@@ -7,7 +7,6 @@ import (
 
 func (p *Parser) parseArrayLiteral() ast.Expression {
 	array := &ast.ArrayLiteral{Token: p.curToken}
-
 	array.Elements = p.parseExpressionList(token.RBRACKET)
 
 	return array
