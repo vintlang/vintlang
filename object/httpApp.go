45230cef341789494ec379ad29017c8c845a211e
tacheraSasi <tacherasasi@gmail.com>
2025-08-09 07:16:57 +0000

    Complete Express.js-like HTTP module with examples and tests
    Co-authored-by: tacheraSasi <141908385+tacheraSasi@users.noreply.github.com>

--- object/httpApp.go
+++ object/httpApp.go
@@ -28,22 +28,48 @@ func (app *HTTPApp) Inspect() string {
 
 // HTTPRequest represents an HTTP request
 type HTTPRequest struct {
-	Method  string
-	Path    string
-	Headers map[string]string
-	Body    string
-	Query   map[string]string
-	Params  map[string]string
+	HTTPMethod string
+	Path       string
+	Headers    map[string]string
+	Body       string
+	Query      map[string]string
+	Params     map[string]string
 }
 
 func (req *HTTPRequest) Type() ObjectType { return HTTP_REQUEST_OBJ }
 func (req *HTTPRequest) Inspect() string {
 	var out bytes.Buffer
 	out.WriteString("HTTPRequest{")
-	out.WriteString(fmt.Sprintf("method: %s, ", req.Method))
+	out.WriteString(fmt.Sprintf("method: %s, ", req.HTTPMethod))
 	out.WriteString(fmt.Sprintf("path: %s", req.Path))
 	out.WriteString("}")
 	return out.String()
+}
+
+// Method to access request properties
+func (req *HTTPRequest) Method(name string, args []Object) Object {
+	switch name {
+	case "get":
+		if len(args) != 1 {
+			return &Error{Message: "req.get() requires 1 argument: header name"}
+		}
+		headerName, ok := args[0].(*String)
+		if !ok {
+			return &Error{Message: "Header name must be a string"}
+		}
+		if value, exists := req.Headers[headerName.Value]; exists {
+			return &String{Value: value}
+		}
+		return &String{Value: ""}
+	case "body":
+		return &String{Value: req.Body}
+	case "method":
+		return &String{Value: req.HTTPMethod}
+	case "path":
+		return &String{Value: req.Path}
+	default:
+		return &Error{Message: fmt.Sprintf("Unknown request method: %s", name)}
+	}
 }
 
 // HTTPResponse represents an HTTP response
@@ -62,6 +88,60 @@ func (res *HTTPResponse) Inspect() string {
 	out.WriteString(fmt.Sprintf("status: %d", res.StatusCode))
 	out.WriteString("}")
 	return out.String()
+}
+
+// Method to access response functionality
+func (res *HTTPResponse) Method(name string, args []Object) Object {
+	switch name {
+	case "send":
+		if len(args) != 1 {
+			return &Error{Message: "res.send() requires 1 argument: message"}
+		}
+		message, ok := args[0].(*String)
+		if !ok {
+			return &Error{Message: "Message must be a string"}
+		}
+		res.Send(message.Value)
+		return &String{Value: "Response sent"}
+	case "json":
+		if len(args) != 1 {
+			return &Error{Message: "res.json() requires 1 argument: data"}
+		}
+		// For now, convert the object to its string representation
+		// In a full implementation, this would properly serialize to JSON
+		data := args[0].Inspect()
+		res.Writer.Header().Set("Content-Type", "application/json")
+		for key, value := range res.Headers {
+			res.Writer.Header().Set(key, value)
+		}
+		res.Writer.WriteHeader(res.StatusCode)
+		res.Writer.Write([]byte(data))
+		res.Sent = true
+		return &String{Value: "JSON response sent"}
+	case "status":
+		if len(args) != 1 {
+			return &Error{Message: "res.status() requires 1 argument: status code"}
+		}
+		statusCode, ok := args[0].(*Integer)
+		if !ok {
+			return &Error{Message: "Status code must be an integer"}
+		}
+		res.StatusCode = int(statusCode.Value)
+		return res // Return self for chaining
+	case "header":
+		if len(args) != 2 {
+			return &Error{Message: "res.header() requires 2 arguments: key and value"}
+		}
+		key, ok1 := args[0].(*String)
+		value, ok2 := args[1].(*String)
+		if !ok1 || !ok2 {
+			return &Error{Message: "Header key and value must be strings"}
+		}
+		res.Headers[key.Value] = value.Value
+		return res // Return self for chaining
+	default:
+		return &Error{Message: fmt.Sprintf("Unknown response method: %s", name)}
+	}
 }
 
 // Helper function to create a new HTTPApp
@@ -94,12 +174,12 @@ func NewHTTPRequest(r *http.Request) *HTTPRequest {
 	}
 
 	return &HTTPRequest{
-		Method:  r.Method,
-		Path:    r.URL.Path,
-		Headers: headers,
-		Body:    string(bodyBytes),
-		Query:   query,
-		Params:  make(map[string]string),
+		HTTPMethod: r.Method,
+		Path:       r.URL.Path,
+		Headers:    headers,
+		Body:       string(bodyBytes),
+		Query:      query,
+		Params:     make(map[string]string),
 	}
 }
 
