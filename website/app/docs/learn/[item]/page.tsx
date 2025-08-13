f6fa54df4bd515685205717f9aa848902bacd6c3
tacheraSasi <tacherasasi@gmail.com>
2025-08-13 14:36:33 +0300

    Refactor LearnItem component to dynamically fetch and display markdown based on item parameter

--- website/app/docs/learn/[item]/page.tsx
+++ website/app/docs/learn/[item]/page.tsx
@@ -1,21 +1,20 @@
 "use client";
 
-import SectionHeader from '@/components/docs/SectionHeader';
-import { fetchMarkdown } from '@/lib/utils';
-import { useParams } from 'next/navigation'
-import React from 'react'
-import Markdown from 'react-markdown';
+import LearnItem from "@/components/docs/learn-item";
+import SectionHeader from "@/components/docs/SectionHeader";
+import { fetchMarkdown } from "@/lib/utils";
+import { useParams } from "next/navigation";
+import React from "react";
+import Markdown from "react-markdown";
 
 const Page = () => {
-  const params = useParams<{ item: string }>()
-  console.log(params)
-  const markdown = await fetchMarkdown("docs/bundler.md");
-    return (
-      <div className="p-6" id="docs">
-        <SectionHeader title="Bundler" />
-        <Markdown>{markdown}</Markdown>
-      </div>
-    )
+  const params = useParams<{ item: string }>();
+  console.log(params);
+  return (
+    <div className="space-y-10">
+      <LearnItem item={params.item} />
+    </div>
+  );
 };
 
-export default Page
+export default Page;
