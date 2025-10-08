423ad03ccf8ead5b7e0092fa8f900a24ac447ae9
Tachera Sasi <tacherasasi@gmail.com>
2025-10-08 12:54:36 +0300

    Refactor builtins initialization and enhance debounce tests with proper execution checks

--- evaluator/builtins/init.go
+++ evaluator/builtins/init.go
@@ -3,9 +3,7 @@ package builtins
 // This file ensures all builtin submodules are initialized
 // by having their init() functions called when the package is imported.
 
-import (
-	// All init functions will be called automatically when the package is imported
-)
+// All init functions will be called automatically when the package is imported
 
 // Additional initialization can be done here if needed
 func init() {
