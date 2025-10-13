357848f5a8f9896be3271e87c1a63e8a2cd0edc5
Tachera Sasi <tacherasasi@gmail.com>
2025-10-13 09:17:36 +0300

    Refactor package evaluation to track private identifiers and enhance package structure

--- evaluator/package.go
+++ evaluator/package.go
@@ -1,20 +1,24 @@
 package evaluator
 
 import (
+	"strings"
+
 	"github.com/vintlang/vintlang/ast"
 	"github.com/vintlang/vintlang/object"
 )
 
 func evalPackage(node *ast.Package, env *object.Environment) object.VintObject {
 	Package := &object.Package{
-		Name:  node.Name,
-		Env:   env,
-		Scope: object.NewEnclosedEnvironment(env),
+		Name:         node.Name,
+		Env:          env,
+		Scope:        object.NewEnclosedEnvironment(env),
+		PrivateNames: make(map[string]bool),
 	}
 
 	Package.Scope.Define("@", Package)
 
-	Eval(node.Block, Package.Scope)
+	// Evaluate package contents and track private identifiers
+	evalPackageContents(node.Block, Package)
 
 	// Automatically run the init function if it exists
 	if initFunc, ok := Package.Scope.Get("init"); ok {
@@ -22,6 +26,33 @@ func evalPackage(node *ast.Package, env *object.Environment) object.VintObject {
 			applyFunction(fn, []object.VintObject{}, 0)
 		}
 	}
+	
 	env.Define(node.Name.Value, Package)
 	return Package
+}
+
+// evalPackageContents evaluates package contents and tracks private members
+func evalPackageContents(block *ast.BlockStatement, pkg *object.Package) object.VintObject {
+	var result object.VintObject
+	
+	for _, statement := range block.Statements {
+		result = Eval(statement, pkg.Scope)
+		if result != nil && result.Type() == object.ERROR_OBJ {
+			return result
+		}
+		
+		// Track private identifiers based on naming convention
+		switch stmt := statement.(type) {
+		case *ast.LetStatement:
+			if strings.HasPrefix(stmt.Name.Value, "_") {
+				pkg.DefinePrivate(stmt.Name.Value)
+			}
+		case *ast.ConstStatement:
+			if strings.HasPrefix(stmt.Name.Value, "_") {
+				pkg.DefinePrivate(stmt.Name.Value)
+			}
+		}
+	}
+	
+	return result
 }
