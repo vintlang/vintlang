d8058ab2428c3689c7f2ec6e6361a8d4fbd7c0fd
Tachera Sasi <tacherasasi@gmail.com>
2025-11-25 14:19:32 +0300

    feat: Add functions to read and print total lines from count.txt

--- toolkit/count.go
+++ toolkit/count.go
@@ -2,11 +2,28 @@ package toolkit
 
 import (
 	"bufio"
+	"embed"
 	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 )
+
+//go:embed count.txt
+var CountFile embed.FS
+
+func PrintCodebaseLines() {
+	line := GetCountFile()
+	fmt.Println("TOTAL LINES OF CODE:", line)
+}
+
+func GetCountFile() string {
+	content, err := CountFile.ReadFile("count.txt")
+	if err != nil {
+		return ""
+	}
+	return string(content)
+}
 
 // Lines counts and prints the total number of lines in files with specific extensions in the current directory and its subdirectories.
 func Lines() {
