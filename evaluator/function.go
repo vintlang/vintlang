413bc80ad573f66033f711bad37c99670ffa4c70
tacheraSasi <tacherasasi@gmail.com>
2026-07-27 11:43:27 +0300

    evaluator: add evalTypedFunction handler for TypedFunctionLiteral

--- evaluator/function.go
+++ evaluator/function.go
@@ -16,3 +16,31 @@ func evalFunction(node *ast.FunctionLiteral, env *object.Environment) object.Vin
 
 	return function
 }
+
+func evalTypedFunction(node *ast.TypedFunctionLiteral, env *object.Environment) object.VintObject {
+	params := make([]*ast.Identifier, len(node.Parameters))
+	paramTypes := make([]ast.Type, len(node.Parameters))
+	for i, tp := range node.Parameters {
+		params[i] = tp.Identifier
+		paramTypes[i] = tp.Type
+	}
+
+	defaults := make(map[string]ast.Expression)
+	for _, tp := range node.Parameters {
+		if tp.Default != nil {
+			defaults[tp.Identifier.Value] = tp.Default
+		}
+	}
+
+	function := &object.Function{
+		Name:       "",
+		Parameters: params,
+		ParamTypes: paramTypes,
+		ReturnType: node.ReturnType,
+		Defaults:   defaults,
+		Body:       node.Body,
+		Env:        env,
+	}
+
+	return function
+}
